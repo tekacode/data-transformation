a331b12caa4cb3be4fa833f5c9b091585bde1c67
Tekalegn Fisseha <tekalegn@minwo.co>
2022-01-26 12:03:50 -0500

    code refactored

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,14 +3,14 @@ public class Person {
     int age;
     String city;
     String flags;
-    // Constructor
+    // First constructor
     public Person(String name, int age, String city, String flags){
         this.name = name;
         this.age = age;
         this.city = city;
         this.flags = flags;
     }
-    // Constructor
+    // Second constructor
     public Person(String name, String flags, String city){
         this.name = name;
         this.flags = flags;
--- src/main/java/Transformation.java
+++ src/main/java/Transformation.java
@@ -9,6 +9,7 @@ public class Transformation {
         hm.put(2, new Person("Jane Doe","YNY", "N Kingsville, OH"));
         hm.put(3, new Person("Sally Jones",25,"Paris", "YYY"));
 
+        // Covery HashMap object to entrySet
         var entrySet = hm.entrySet();
         for(var entry: entrySet){
             String gender;
@@ -17,28 +18,24 @@ public class Transformation {
 
             String[] addressArray = entry.getValue().city.split(" ");
             String city = addressArray[0];
+            if(city.length() == 1){
+                city = addressArray[0]+ " " + addressArray[1];
+            }
             String state = addressArray[addressArray.length-1];
             if(state == "Paris"){
                 state = "N/A";
             }
             String flags = entry.getValue().flags;
-            if(flags.charAt(0) == 'Y'){
-                 gender = "Female";
-            }else{
-                 gender = "Male";
-            }
-            if(flags.charAt(1) == 'Y'){
-                 student = "Yes";
-            }else{
-                 student = "NO";
-            }
-            if(flags.charAt(2) == 'Y'){
-                 employee = "Yes";
-            }else {
-                employee = "No";
-            }
+            gender = flags.charAt(0) == 'Y'? "Female": "Male";
+            student = flags.charAt(1) == 'Y'? "Yes": "No";
+            employee = flags.charAt(2) == 'Y'? "Yes": "No";
 
-            System.out.println(entry.getValue().name + "["+ entry.getValue().age+",Male]");
+
+            if(entry.getValue().age !=0){
+                System.out.println(entry.getValue().name + " ["+ entry.getValue().age+", "+gender+ "]");
+            }else{
+                System.out.println(entry.getValue().name+ " ["+gender+ "]");
+            }
             System.out.println("\t\t\tCity : "+city);
             System.out.println("\t\t\tState : "+ state);
             System.out.println("\t\t\tStudent : "+ student);
